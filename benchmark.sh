a2e8067cfc94d83c7e2585f868e7048febdbb240
H. R. Zohouri <zohouri.h.aa@m.titech.ac.jp>
2019-02-02 16:09:38 +0900

    Update benchmark script

--- benchmark.sh
+++ benchmark.sh
@@ -2,33 +2,48 @@
 
 export CL_CONTEXT_COMPILER_MODE_ALTERA=3
 
-iter=5
+iter=2
 size=1024
 folder=de5net
 
-echo Kernel | xargs printf "%-65s"
+echo "Type" | xargs printf "%-8s"
+echo "Model" | xargs printf "%-8s"
+echo "Cache" | xargs printf "%-8s"
+echo "Interleave" | xargs printf "%-12s"
+echo "Vector" | xargs printf "%-8s"
+echo "Frequency" | xargs printf "%-12s"
 echo "Copy\ (GiB/s)" | xargs printf "%-15s"
-echo "Mac\ (GiB/s)" | xargs printf "%-15s"
+echo "MAC\ (GiB/s)" | xargs printf "%-15s"
 echo
 
 for i in `ls $folder | grep aocx | sort -V`
 do
 	name="${i%.*}"
-	echo "$name" | xargs printf "%-65s"
+	type=`echo $name | cut -d "-" -f 4 | cut -d "_" -f 1 | tr '[:lower:]' '[:upper:]'`
 	if [[ `echo $name | cut -d "_" -f 2` == NDR ]]
 	then
 		ndr=1
+		model=NDR
 	else
 		ndr=0
+		model=SWI
 	fi
 	VEC=`echo $name | cut -d "_" -f 3 | cut -c 4-`
 	if [[ -n `echo $name | grep nointer` ]]
 	then
 		nointer=1
+		inter=NO
 	else
 		nointer=0
+		inter=YES
 	fi
-	#freq=`cat $folder/$name/acl_quartus_report.txt | grep Actual | cut -d " " -f 4`
+	if [[ -n `echo $name | grep nocache` ]]
+	then
+		cache=NO
+	else
+		cache=YES
+	fi
+	freq=`cat $folder/$name/acl_quartus_report.txt | grep Actual | cut -d " " -f 4 | xargs printf %0.2f`
 
 	make clean >/dev/null 2>&1; make INTEL_FPGA=1 HOST_ONLY=1 VEC=$VEC NO_INTER=$nointer NDR=$ndr >/dev/null 2>&1
 	rm fpga-stream-kernel.aocx >/dev/null 2>&1
@@ -40,6 +55,12 @@ do
 	copy=`echo "$out" | grep "Copy:" | cut -d " " -f 2`
 	mac=`echo "$out" | grep "MAC :" | cut -d " " -f 3`
 
+	echo $type | xargs printf "%-8s"
+	echo $model | xargs printf "%-8s"
+	echo $cache | xargs printf "%-8s"
+	echo $inter | xargs printf "%-12s"
+	echo $VEC | xargs printf "%-8s"
+	echo $freq | xargs printf "%-12s"
 	echo $copy | xargs printf "%-15s"
 	echo $mac | xargs printf "%-15s"
 	echo
