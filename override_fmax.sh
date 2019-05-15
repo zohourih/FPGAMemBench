57f31699a4d5b27d7a0fb1c3795ea9dd1e718439
zohourih <zohouri.h.aa@m.titech.ac.jp>
2019-05-15 17:47:02 +0900

    Some error checking for fmax override script

--- override_fmax.sh
+++ override_fmax.sh
@@ -22,7 +22,7 @@ if [[ -z $3 ]]
 then
 	skip=1
 else
-	echo "Overriding Fmax!"
+	echo -n "Overriding Fmax: "
 	fmax=$3
 fi
 
@@ -37,42 +37,62 @@ then
 
 	if [[ $pro -eq 0 ]]
 	then
+		file=`echo $path/scripts/post_flow.tcl`
 		if [[ $legacy -eq 1 ]]
 		then
 			orig=`echo "source $::env(ALTERAOCLSDKROOT)/ip/board/bsp/adjust_plls.tcl"`
 			orig_fix=$(printf '%s\n' "$orig" | sed 's:[\/&]:\\&:g;$!s/$/\\/')
+			if [[ -z `cat $file | grep "$orig"` ]]
+			then
+				echo "FAILURE!! (pattern not found)"
+				exit -1
+			fi
 
 			new=`echo -e "set argv [list -fmax $fmax]\n        set argc 2\n        $orig\n        unset argv\n        unset argc"`
 			new_fix=$(printf '%s\n' "$new" | sed 's:[\/&]:\\&:g;$!s/$/\\/')
-
-			sed -i "s/$orig_fix/$new_fix/" $path/scripts/post_flow.tcl
 		else
 			orig=`echo "source $::env(INTELFPGAOCLSDKROOT)/ip/board/bsp/adjust_plls.tcl"`
 			orig_fix=$(printf '%s\n' "$orig" | sed 's:[\/&]:\\&:g;$!s/$/\\/')
+			if [[ -z `cat $file | grep "$orig"` ]]
+			then
+				echo "FAILURE!! (pattern not found)"
+				exit -1
+			fi
 
 			new=`echo -e "set argv [list -fmax $fmax]\n  set argc 2\n  $orig\n  unset argv\n  unset argc"`
 			new_fix=$(printf '%s\n' "$new" | sed 's:[\/&]:\\&:g;$!s/$/\\/')
-
-			sed -i "s/$orig_fix/$new_fix/" $path/scripts/post_flow.tcl
 		fi
+
+		sed -i "s/$orig_fix/$new_fix/" $file
+		echo "SUCCESS!!"
 	else
+		file=`echo $path/scripts/post_flow_pr.tcl`
 		if [[ $legacy -eq 1 ]]
 		then
 			orig=`echo "source $::env(ALTERAOCLSDKROOT)/ip/board/bsp/adjust_plls_a10.tcl"`
 			orig_fix=$(printf '%s\n' "$orig" | sed 's:[\/&]:\\&:g;$!s/$/\\/')
+			if [[ -z `cat $file | grep "$orig"` ]]
+			then
+				echo "FAILURE!! (pattern not found)"
+				exit -1
+			fi
 
 			new=`echo -e "set argv [list -fmax $fmax]\nset argc 2\n$orig\nunset argv\nunset argc"`
 			new_fix=$(printf '%s\n' "$new" | sed 's:[\/&]:\\&:g;$!s/$/\\/')
-
-			sed -i "s/$orig_fix/$new_fix/" $path/scripts/post_flow_pr.tcl
 		else
 			orig=`echo "source $::env(INTELFPGAOCLSDKROOT)/ip/board/bsp/adjust_plls_a10.tcl"`
 			orig_fix=$(printf '%s\n' "$orig" | sed 's:[\/&]:\\&:g;$!s/$/\\/')
+			if [[ -z `cat $file | grep "$orig"` ]]
+			then
+				echo "FAILURE!! (pattern not found)"
+				exit -1
+			fi
 
 			new=`echo -e "set argv [list -fmax $fmax]\nset argc 2\n$orig\nunset argv\nunset argc"`
 			new_fix=$(printf '%s\n' "$new" | sed 's:[\/&]:\\&:g;$!s/$/\\/')
-
-			sed -i "s/$orig_fix/$new_fix/" $path/scripts/post_flow_pr.tcl
 		fi
+
+		sed -i "s/$orig_fix/$new_fix/" $file
+		echo "SUCCESS!!"
 	fi
 fi
