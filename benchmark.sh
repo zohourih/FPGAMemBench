189798bb9ecb15c2c4ff07051c502168afb73d09
zohourih <zohouri.h.aa@m.titech.ac.jp>
2019-07-05 19:31:55 +0900

    Update benchmark script based on recent changes

--- benchmark.sh
+++ benchmark.sh
@@ -3,7 +3,7 @@
 export CL_CONTEXT_COMPILER_MODE_ALTERA=3
 
 iter=5
-size=768
+size=1024
 indexes=$(($size * 256 * 1024))
 sqrt=`echo "e(l($indexes)/2)" | bc -l`
 cbrt=`echo "e(l($indexes)/3)" | bc -l`
@@ -13,7 +13,7 @@ dim_x_3d=`echo "x=l($cbrt)/l(2); scale=0; if (x%1 != 0) x = (x + 1); x = (x / 1)
 dim_y_3d=$dim_x_3d
 dim_z_3d=$(( $indexes / ($dim_x_3d * dim_y_3d) ))
 size_switch=""
-board=`aoc --list-boards | sed -n 2p | tr -d ' ' | cut -d "_" -f 1`
+board=`aoc --list-boards | grep Board -A 2 | sed -n 2p | tr -d ' ' | cut -d "_" -f 1`
 if [[ "$board" == "de5net" ]]
 then
 	max_bw="25.6"
@@ -30,7 +30,7 @@ overlap=0
 halo_start=0
 halo_end=0
 halo_step=1
-overlap_switch=""
+halo_switch=""
 if [[ "$1" == "--verify" ]] || [[ "$2" == "--verify" ]] || [[ "$3" == "--verify" ]]
 then
 	verify="--verify"
@@ -45,24 +45,26 @@ then
 fi
 
 echo "Type" | xargs printf "%-9s"
-echo "Model" | xargs printf "%-8s"
+echo "Model" | xargs printf "%-7s"
 echo "Cache" | xargs printf "%-7s"
 echo "Inter." | xargs printf "%-8s"
-echo "VEC" | xargs printf "%-6s"
+echo "VEC" | xargs printf "%-5s"
 echo "Freq." | xargs printf "%-9s"
 echo "Size" | xargs printf "%-15s"
 echo "Pad" | xargs printf "%-6s"
-echo "O\\\H" | xargs printf "%-6s"
-echo "Copy\ (GB/s)" | xargs printf "%-13s"
-echo "MAC\ (GB/s)" | xargs printf "%-12s"
-echo "Copy\ Eff." | xargs printf "%-11s"
-echo "MAC\ Eff." | xargs printf "%-8s"
+echo "Halo" | xargs printf "%-6s"
+echo "Performance\ (GB/s)" | xargs printf "%-36s"
+echo "Efficiency\ (%)" | xargs printf "%-26s"
+if [[ "$verify" == "--verify" ]]
+then
+	echo "Verification" | xargs printf "%-11s"
+fi
 echo
 
 for i in `ls $folder | grep aocx | sort -V`
 do
 	name="${i%.*}"
-	type=`echo $name | cut -d "-" -f 4 | cut -d "_" -f 1`
+	type=`echo $name | cut -d "-" -f 5 | cut -d "_" -f 1`
 
 	if [[ "$type" == "blk3d" ]] || [[ "$type" == "chblk3d" ]]
 	then
@@ -113,19 +115,19 @@ do
 		dim_x_3d_aligned=$(( ($dim_x_3d % $compute_bsize_x) == 0 ? $dim_x_3d : ($dim_x_3d + $compute_bsize_x - $dim_x_3d % $compute_bsize_x) ))
 		dim_y_3d_aligned=$(( ($dim_y_3d % $compute_bsize_y) == 0 ? $dim_y_3d : ($dim_y_3d + $compute_bsize_y - $dim_y_3d % $compute_bsize_y) ))
 
-		if [[ "$type" == "std" ]]
+		if [[ "$type" == "std" ]] || [[ "$type" == "chstd" ]]
 		then
-			overlap_switch="-o $overlap"
+			halo_switch="-hw $halo"
 			size_switch="-s $size"
 			dim=$size
 		elif [[ "$type" == "blk2d" ]] || [[ "$type" == "chblk2d" ]]
 		then
-			overlap_switch="-hw $halo"
+			halo_switch="-hw $halo"
 			size_switch="-x $dim_x_2d_aligned -y $dim_y_2d"
 			dim=`echo "$dim_x_2d_aligned"x"$dim_y_2d"`
 		elif [[ "$type" == "blk3d" ]] || [[ "$type" == "chblk3d" ]]
 		then
-			overlap_switch="-hw $halo"
+			halo_switch="-hw $halo"
 			size_switch="-x $dim_x_3d_aligned -y $dim_y_3d_aligned -z $dim_z_3d"
 			dim=`echo "$dim_x_3d_aligned"x"$dim_y_3d_aligned"x"$dim_z_3d"`
 		else
@@ -135,57 +137,92 @@ do
 
 		for ((pad = $pad_start ; pad <= $pad_end ; pad++))
 		do
-			out=`DEVICE_TYPE=FPGA ./fpga-mem-bench $size_switch -n $iter -pad $pad $overlap_switch $verify 2>&1`
+			out=`DEVICE_TYPE=FPGA ./fpga-mem-bench $size_switch -n $iter -pad $pad $halo_switch $verify 2>&1`
 			#echo "$out" >> ast.txt
 
-			copy=`echo "$out" | grep "Copy:" | cut -d " " -f 2`
-			mac=`echo "$out" | grep "MAC :" | cut -d " " -f 3`
+			R1W1=`echo "$out" | grep "R1W1:" | cut -d " " -f 2`
+			R2W1=`echo "$out" | grep "R2W1:" | cut -d " " -f 2`
+			R3W1=`echo "$out" | grep "R3W1:" | cut -d " " -f 2`
+			R2W2=`echo "$out" | grep "R2W2:" | cut -d " " -f 2`
 
-			bw_copy=`echo "$freq * $VEC * 4 * 2 / 1000" | bc -l | xargs printf %0.3f`
-			if [[ `echo "$bw_copy > $max_bw" | bc -l` -eq 1 ]]
+			bw_R1W1=`echo "$freq * $VEC * 4 * 2 / 1000" | bc -l | xargs printf %0.3f`
+			if [[ `echo "$bw_R1W1 > $max_bw" | bc -l` -eq 1 ]]
 			then
-				bw_copy=$max_bw
+				bw_R1W1=$max_bw
 			fi
-			bw_mac=`echo "$freq * $VEC * 4 * 3 / 1000" | bc -l | xargs printf %0.3f`
-			if [[ `echo "$bw_mac > $max_bw" | bc -l` -eq 1 ]]
+			bw_R2W1=`echo "$freq * $VEC * 4 * 3 / 1000" | bc -l | xargs printf %0.3f`
+			if [[ `echo "$bw_R2W1 > $max_bw" | bc -l` -eq 1 ]]
 			then
-				bw_mac=$max_bw
+				bw_R2W1=$max_bw
 			fi
-			copy_eff=`echo "100 * ($copy/$bw_copy)" | bc -l | xargs printf %0.1f`
-			mac_eff=`echo "100 * ($mac/$bw_mac)" | bc -l | xargs printf %0.1f`
-
-			copy_ver=`echo "$out" | grep Verify | grep Copy | cut -d " " -f 4 | cut -c 1-1`
-			mac_ver=`echo "$out" | grep Verify | grep MAC | cut -d " " -f 4 | cut -c 1-1`
-
-			if [[ -n `echo "$out" | grep Halo` ]]
+			bw_R3W1=`echo "$freq * $VEC * 4 * 4 / 1000" | bc -l | xargs printf %0.3f`
+			if [[ `echo "$bw_R3W1 > $max_bw" | bc -l` -eq 1 ]]
 			then
-				halo_overlap=`echo "$out" | grep "Halo" | tr -s " " | cut -d " " -f 3`
-			elif [[ -n `echo "$out" | grep Overlap` ]]
+				bw_R3W1=$max_bw
+			fi
+			bw_R2W2=`echo "$freq * $VEC * 4 * 4 / 1000" | bc -l | xargs printf %0.3f`
+			if [[ `echo "$bw_R2W2 > $max_bw" | bc -l` -eq 1 ]]
 			then
-				halo_overlap=`echo "$out" | grep "Overlap" | tr -s " " | cut -d " " -f 2`
+				bw_R2W2=$max_bw
+			fi
+
+			R1W1_eff=`echo "100 * ($R1W1/$bw_R1W1)" | bc -l | xargs printf %0.1f`
+			R2W1_eff=`echo "100 * ($R2W1/$bw_R2W1)" | bc -l | xargs printf %0.1f`
+			R3W1_eff=`echo "100 * ($R3W1/$bw_R3W1)" | bc -l | xargs printf %0.1f`
+			R2W2_eff=`echo "100 * ($R2W2/$bw_R2W2)" | bc -l | xargs printf %0.1f`
+
+			R1W1_ver=`echo "$out" | grep Verify | grep R1W1 | cut -d " " -f 4 | cut -c 1-1`
+			R2W1_ver=`echo "$out" | grep Verify | grep R2W1 | cut -d " " -f 4 | cut -c 1-1`
+			R3W1_ver=`echo "$out" | grep Verify | grep R3W1 | cut -d " " -f 4 | cut -c 1-1`
+			R2W2_ver=`echo "$out" | grep Verify | grep R2W2 | cut -d " " -f 4 | cut -c 1-1`
+
+			if [[ "$type" == "std" ]] || [[ "$type" == "chstd" ]]
+			then
+				R1W0=`echo "$out" | grep "R1W0:" | cut -d " " -f 2`
+
+				bw_R1W0=`echo "$freq * $VEC * 4 * 1 / 1000" | bc -l | xargs printf %0.3f`
+				if [[ `echo "$bw_R1W0 > $max_bw" | bc -l` -eq 1 ]]
+				then
+					bw_R1W0=$max_bw
+				fi
+
+				R1W0_eff=`echo "100 * ($R1W0/$bw_R1W0)" | bc -l | xargs printf %0.1f`
+
+				R1W0_ver=`echo "N/A"`
+			fi
+
+			if [[ "$type" != "sch" ]]
+			then
+				halo_size=$halo
 			else
-				halo_overlap="N/A"
+				halo_size="N/A"
 			fi
 
 			echo $type  | tr '[:lower:]' '[:upper:]' | xargs printf "%-9s"
-			echo $model | xargs printf "%-8s"
+			echo $model | xargs printf "%-7s"
 			echo $cache | xargs printf "%-7s"
 			echo $inter | xargs printf "%-8s"
-			echo $VEC | xargs printf "%-6s"
+			echo $VEC | xargs printf "%-5s"
 			echo $freq | xargs printf "%-9s"
 			echo $dim | xargs printf "%-15s"
 			echo $pad | xargs printf "%-6s"
-			echo $halo_overlap | xargs printf "%-6s"
-			if [[ "$verify" == "--verify" ]]
+			echo $halo_size | xargs printf "%-6s"
+			if [[ "$type" == "std" ]] || [[ "$type" == "chstd" ]]
 			then
-				echo "$copy\ ($copy_ver)" | xargs printf "%-13s"
-				echo "$mac\ ($mac_ver)" | xargs printf "%-12s"
+				echo "$R1W0|$R1W1|$R2W1|$R3W1|$R2W2" | xargs printf "%-36s"
+				echo "$R1W0_eff|$R1W1_eff|$R2W1_eff|$R3W1_eff|$R2W2_eff" | xargs printf "%-26s"
+				if [[ "$verify" == "--verify" ]]
+				then
+					echo "$R1W0_ver|$R1W1_ver|$R2W1_ver|$R3W1_ver|$R2W2_ver" | xargs printf "%-11s"
+				fi
 			else
-				echo $copy | xargs printf "%-13s"
-				echo $mac | xargs printf "%-12s"
+				echo "$R1W1|$R2W1|$R3W1|$R2W2" | xargs printf "%-36s"
+				echo "$R1W1_eff|$R2W1_eff|$R3W1_eff|$R2W2_eff" | xargs printf "%-26s"
+				if [[ "$verify" == "--verify" ]]
+				then
+					echo "$R1W1_ver|$R2W1_ver|$R3W1_ver|$R2W2_ver" | xargs printf "%-11s"
+				fi
 			fi
-			echo "$copy_eff%" | xargs printf "%-11s"
-			echo "$mac_eff%" | xargs printf "%-8s"
 			echo
 		done
 	done
